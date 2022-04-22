c353d77b0dd6f6ad34d5aeb2cde70f2cfe0d00f3
aleksandrbelaev <alexanderbelyaev1990@gmail.com>
2022-04-22 17:25:34 +0200

    добавил static

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,17 +1,16 @@
 package org.example;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.example.configuration.MyConfig;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 public class App {
-    public static void main(String[] args) throws JsonProcessingException {
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
-        Communication communication = context.getBean("communication", Communication.class);
-        communication.getAllUsersEntity();
-
+    public static void main(String[] args) {
+        Response response = new Response();
+        response.getAll();
+        response.addUser();
+        response.editUser();
+        response.delUser();
     }
 
-
 }
+
+
+
--- src/main/java/org/example/Response.java
+++ src/main/java/org/example/Response.java
@@ -1,2 +1,76 @@
-package org.example;public class Response {
+package org.example;
+
+import org.example.entity.User;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class Response {
+    List<String> cookies = null;
+    static final String baseUrl = "http://94.198.50.185:7081/api/users";
+    static final  RestTemplate restTemplate = new RestTemplate();
+    User sysUser = new User();
+
+    ResponseEntity<String> getAll() {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(baseUrl, //
+                HttpMethod.GET, entity, String.class);
+
+        String result = response.getBody();
+
+        System.out.println(result);
+        System.out.println(response);
+        System.out.println(response.getHeaders().get("Set-Cookie"));
+        cookies = response.getHeaders().get("Set-Cookie");
+
+        return response;
+    }
+
+    public void addUser() {
+
+        sysUser.setName("James");
+        sysUser.setLastName("Brown");
+        sysUser.setAge((byte) 30);
+        sysUser.setId(3L);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Cookie",cookies.stream().collect(Collectors.joining(";")));
+        HttpEntity<User> requestBody = new HttpEntity<>(sysUser, headers);
+
+        ResponseEntity<String> responseAdd =  restTemplate.exchange("http://94.198.50.185:7081/api/users", HttpMethod.POST, requestBody, String.class);
+        System.out.println(responseAdd.getBody());
+        System.out.println(cookies);
+    }
+
+    public void editUser() {
+
+        sysUser.setName("Thomas");
+        sysUser.setLastName("Shelby");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Cookie",cookies.stream().collect(Collectors.joining(";")));
+        HttpEntity<User> requestBody = new HttpEntity<>(sysUser,headers);
+
+        ResponseEntity<String> responseAdd =  restTemplate.exchange("http://94.198.50.185:7081/api/users", HttpMethod.PUT, requestBody, String.class);
+        System.out.println(responseAdd.getBody());
+        System.out.println(cookies);
+    }
+
+    public void delUser(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Cookie",cookies.stream().collect(Collectors.joining(";")));
+        HttpEntity<User> requestBody = new HttpEntity<>(sysUser,headers);
+
+        ResponseEntity<String> responseAdd =  restTemplate.exchange("http://94.198.50.185:7081/api/users/3", HttpMethod.DELETE, requestBody, String.class);
+        System.out.println(responseAdd.getBody());
+        System.out.println(cookies);
+    }
 }
